98b03b006048e72d336c888b2ad25c2f979c4432
Isabel Huerta <289063917+isandersonal-arch@users.noreply.github.com>
2026-07-09 19:25:15 +0000

    Added an event listener and a console message when the Image button is clicked/image retrieved.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ const endInput = document.getElementById('endDate');
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
 setupDateInputs(startInput, endInput);
+
+//Select the button and gallery container elements from the HTML
+const imageBtn = document.getElementById('image-btn');
+
+// Add an Event Listener to the Button
+imageBtn.addEventListener('click', function () {
+  console.log('Retrieving space images...');
+})
